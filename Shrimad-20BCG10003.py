65fb847f725130ff35025a9193db1cef890a6dd2
Shrimad-Bhagwat <sybbhagwat010102@gmail.com>
2021-10-03 12:11:58 +0530

    added Shrimad-20BCG10003.py

---
+++ Shrimad-20BCG10003.py
@@ -0,0 +1,17 @@
+'''
+@ Shrimad Bhagwat 20BCG10003
+
+Program to Convert Integer to Binary
+
+'''
+def binary(num):
+    while (num > 0):
+        temp = num % 2
+        bin_no.append(temp)
+        num = num // 2
+bin_no = []   
+n = int(input("Enter a number : "))
+binary(n)
+print("The binary of ",n," is ",end=" ")
+for i in range (1,len(bin_no)+1):
+        print(bin_no[-i],end="")
