3733887e106167676d17fd9bc7fb8ba9938b317e
Shashwat Tiwari <shashwattiwary@gmail.com>
2023-06-16 15:08:19 +0530

    first java code of the repo

---
+++ Shashwat21BCE11378.java
@@ -0,0 +1,24 @@
+package OpenSourcEd_HandsOn;
+
+public class Shashwat21BCE11378{
+    public static void main(String[] args) {
+        String[] anArray = new String[11];{
+
+        anArray[0] = "Hello";
+        anArray[1] = "We";
+        anArray[2] = "are";
+        anArray[3] = "going";
+        anArray[4] = "to";
+        anArray[5] = "make";
+        anArray[6] = "it";
+        anArray[7] = "out";
+        anArray[8] = "of";
+        anArray[9] = "here";
+        anArray[10] = "soon:)";
+
+        System.out.println(String.join(" ", anArray));
+
+    }
+    }
+        
+}
